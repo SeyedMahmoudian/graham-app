a0f80148f8c8e9507d37dd080e7ec81a421cd395
Amin <amin.s.mahmoudian@gmail.com>
2026-06-08 09:38:38 -0400

    width issue on mobile

--- codes/app.py
+++ codes/app.py
@@ -68,7 +68,7 @@ app.index_string = app.index_string.replace(
 app.index_string = app.index_string.replace(
     '</head>',
     '<script>'
-    'const APP_VERSION = "v3";'  # bump this on each deploy
+    'const APP_VERSION = "v3.1";'  # bump this on each deploy
     'if (localStorage.getItem("app_version") !== APP_VERSION) {'
     '    localStorage.setItem("app_version", APP_VERSION);'
     '    location.reload(true);'
